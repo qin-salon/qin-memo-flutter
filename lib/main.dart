a007e1934d9ffd8b816e5bca45a3b17be2628841
abc123931 <abc123931@gmail.com>
2021-03-26 12:18:01 +0900

    feat: Add Home page.

--- lib/main.dart
+++ lib/main.dart
@@ -6,60 +6,154 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  final TextTheme appBarTextTheme = Typography.material2018().black;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Qin Memo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+            elevation: 0,
+            backgroundColor: Colors.white,
+            textTheme: appBarTextTheme.copyWith(
+                headline6: appBarTextTheme.headline6
+                    .copyWith(fontSize: 18, fontWeight: FontWeight.bold))),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        title: const Text('ホーム'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => print('hello'),
+            child: Container(
+              width: 36,
+              height: 36,
+              child: const CircleAvatar(backgroundImage: NetworkImage(
+                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => print('hello'),
+        label: const Text(
+          'メモを書く',
+          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+        ),
+        icon: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 13, horizontal: 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const <Widget>[
+                      Padding(
+                          padding: EdgeInsets.only(right: 10.5),
+                          child: Icon(
+                            Icons.search,
+                            color: Color(0xFF3B82F6),
+                          )),
+                      Text(
+                        'メモを書く',
+                        style: TextStyle(color: Color(0xFFC2C6D2)),
+                      ),
+                    ],
+                  )),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(999),
+                color: const Color(0xFFF1F5F9),
+              ),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Flexible(
+              child: ListView.separated(
+                separatorBuilder: (BuildContext context, int index) {
+                  return const SizedBox(height: 16);
+                },
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          const Text(
+                            'タイトル',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          const Text('ほそく'),
+                          const SizedBox(
+                            height: 24,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              const Text(
+                                '2021/03/25',
+                                style: TextStyle(color: Color(0xFFC2C6D2)),
+                              ),
+                              Container(
+                                child: const Text(
+                                  '公開中',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 4, horizontal: 12),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(9999),
+                                  color: const Color(0xFFFB923C),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 16, horizontal: 24),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          color: const Color(0xFFF1F5F9),
+                          boxShadow: const <BoxShadow>[
+                            BoxShadow(
+                                color: Color.fromRGBO(0, 0, 0, 0.12),
+                                offset: Offset(0, 1),
+                                blurRadius: 1)
+                          ]),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 24),
       ),
     );
   }
