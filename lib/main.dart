7bc87e75a440f1907a44e30ef89dfbf7efb47841
abc123931 <abc123931@gmail.com>
2021-03-28 00:02:24 +0900

    feat: Add navigation to search page.

--- lib/main.dart
+++ lib/main.dart
@@ -40,8 +40,10 @@ class HomePage extends StatelessWidget {
             child: Container(
               width: 36,
               height: 36,
-              child: const CircleAvatar(backgroundImage: NetworkImage(
-                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),),
+              child: const CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+              ),
             ),
           )
         ],
@@ -58,28 +60,56 @@ class HomePage extends StatelessWidget {
       body: Container(
         child: Column(
           children: <Widget>[
-            Container(
-              child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 13, horizontal: 0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const <Widget>[
-                      Padding(
-                          padding: EdgeInsets.only(right: 10.5),
-                          child: Icon(
-                            Icons.search,
-                            color: Color(0xFF3B82F6),
-                          )),
-                      Text(
-                        'メモを書く',
-                        style: TextStyle(color: Color(0xFFC2C6D2)),
+            TextButton(
+              onPressed: () => <Future<SearchPage>>{
+                Navigator.of(context).push<SearchPage>(
+                  PageRouteBuilder<SearchPage>(
+                    pageBuilder: (_, __, ___) =>
+                        SearchPage(),
+                    transitionsBuilder: (
+                      BuildContext context,
+                      Animation<double> animation,
+                      Animation<double> secondaryAnimation,
+                      Widget child,
+                    ) =>
+                        ScaleTransition(
+                      scale: Tween<double>(
+                        begin: 0.0,
+                        end: 1.0,
+                      ).animate(
+                        CurvedAnimation(
+                          parent: animation,
+                          curve: Curves.fastOutSlowIn,
+                        ),
                       ),
-                    ],
-                  )),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(999),
-                color: const Color(0xFFF1F5F9),
+                      child: child,
+                    ),
+                  ),
+                ),
+              },
+              child: Container(
+                child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 13, horizontal: 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget>[
+                        Padding(
+                            padding: EdgeInsets.only(right: 10.5),
+                            child: Icon(
+                              Icons.search,
+                              color: Color(0xFF3B82F6),
+                            )),
+                        Text(
+                          'メモを検索する',
+                          style: TextStyle(color: Color(0xFFC2C6D2)),
+                        ),
+                      ],
+                    )),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(999),
+                  color: const Color(0xFFF1F5F9),
+                ),
               ),
             ),
             const SizedBox(
@@ -151,6 +181,27 @@ class HomePage extends StatelessWidget {
                 },
               ),
             ),
+          ],
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 24),
+      ),
+    );
+  }
+}
+
+class SearchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('検索'),
+      ),
+      body: Container(
+        child: Column(
+          children: const <Widget>[
+            Text('検索履歴1'),
+            Text('検索履歴2'),
+            Text('検索履歴3'),
           ],
         ),
         padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 24),
