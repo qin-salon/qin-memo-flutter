5ca16936e9aca7e54d222ba94bf2f2706870df17
abc123931 <abc123931@gmail.com>
2021-03-29 00:22:57 +0900

    feat: Add profile modal.

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,143 @@ class HomePage extends StatelessWidget {
         title: const Text('ホーム'),
         actions: <Widget>[
           GestureDetector(
-            onTap: () => print('hello'),
+            onTap: () => <void>{
+              showGeneralDialog(
+                barrierLabel: 'Dismiss',
+                barrierDismissible: true,
+                barrierColor: Colors.black.withOpacity(0.5),
+                transitionDuration: const Duration(milliseconds: 200),
+                context: context,
+                pageBuilder: (BuildContext context, _, __) {
+                  return Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      height: 228,
+                      child: Column(
+                        children: <Widget>[
+                          const SizedBox(height: 8),
+                          Container(
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Container(
+                                  width: 48,
+                                  height: 48,
+                                  child: const CircleAvatar(
+                                    backgroundImage: NetworkImage(
+                                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                                  ),
+                                ),
+                                const SizedBox(width: 12),
+                                Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const <Widget>[
+                                    Text(
+                                      'しまぶー',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color(0xFF070417),
+                                        decoration: TextDecoration.none,
+                                      ),
+                                    ),
+                                    Text(
+                                      '@shimabuit',
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Color(0xFFC2C6D2),
+                                        decoration: TextDecoration.none,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 16, horizontal: 0),
+                          ),
+                          Container(
+                            height: 44,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: const <Widget>[
+                                Icon(Icons.settings_outlined),
+                                SizedBox(width: 16),
+                                Text(
+                                  '設定',
+                                  style: TextStyle(
+                                      color: Color(0xFF070417),
+                                      fontSize: 14,
+                                      decoration: TextDecoration.none),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            height: 44,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: const <Widget>[
+                                Icon(Icons.help_outline_outlined),
+                                SizedBox(width: 16),
+                                Text(
+                                  'ヘルプ',
+                                  style: TextStyle(
+                                      color: Color(0xFF070417),
+                                      fontSize: 14,
+                                      decoration: TextDecoration.none),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            height: 44,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: const <Widget>[
+                                Icon(
+                                  Icons.logout,
+                                  color: Color(0xFFEF4444),
+                                ),
+                                SizedBox(width: 16),
+                                Text(
+                                  'ログアウト',
+                                  style: TextStyle(
+                                      color: Color(0xFFEF4444),
+                                      fontSize: 14,
+                                      decoration: TextDecoration.none),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                        ],
+                      ),
+                      margin:
+                          const EdgeInsets.only(top: 100, left: 16, right: 16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 0, horizontal: 24),
+                    ),
+                  );
+                },
+                transitionBuilder: (BuildContext context,
+                    Animation<double> anim1, _, Widget child) {
+                  return SlideTransition(
+                    position: Tween<Offset>(
+                            begin: const Offset(0, 0),
+                            end: const Offset(0, 0.01))
+                        .animate(anim1),
+                    child: child,
+                  );
+                },
+              )
+            },
             child: Padding(
               padding: const EdgeInsets.only(right: 24),
               child: Container(
