935f2cb7d26c2f9d1fee0d69e882eb38ef00099d
abc123931 <abc123931@gmail.com>
2021-04-02 11:00:45 +0900

    refactor: Refactor dialog.

--- lib/custom_alert_dialog.dart
+++ lib/custom_alert_dialog.dart
@@ -22,73 +22,79 @@ class CustomAlertDialog extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(16),
       ),
-      content: Container(
-        padding: const EdgeInsets.all(24),
-        height: 174,
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              Text(
-                title,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                subText,
-                style: const TextStyle(
-                  fontSize: 14,
-                ),
-              ),
-              const SizedBox(height: 32),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(24),
+            child: Container(
+              child: Column(
                 children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: ElevatedButton(
-                      onPressed: () => Navigator.of(context).pop(),
-                      child: const Text(
-                        'キャンセル',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                          primary: const Color(0xFFF1F5F9),
-                          onPrimary: Colors.black,
-                          shape: const StadiumBorder(),
-                          elevation: 0,
-                          padding: EdgeInsets.zero),
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                    flex: 1,
-                    child: ElevatedButton(
-                      onPressed: action,
-                      child: Text(
-                        actionText,
-                        style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        primary: const Color(0xFFEF4444),
-                        onPrimary: Colors.white,
-                        shape: const StadiumBorder(),
-                        elevation: 0,
-                      ),
+                  Text(
+                    subText,
+                    style: const TextStyle(
+                      fontSize: 14,
                     ),
                   ),
+                  const SizedBox(height: 32),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: ElevatedButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: const Text(
+                            'キャンセル',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xFFF1F5F9),
+                            onPrimary: Colors.black,
+                            shape: const StadiumBorder(),
+                            elevation: 0,
+                            padding: const EdgeInsets.symmetric(vertical: 13),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        flex: 1,
+                        child: ElevatedButton(
+                          onPressed: action,
+                          child: Text(
+                            actionText,
+                            style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              primary: const Color(0xFFEF4444),
+                              onPrimary: Colors.white,
+                              shape: const StadiumBorder(),
+                              elevation: 0,
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 13)),
+                        ),
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
--- lib/profile_dialog.dart
+++ lib/profile_dialog.dart
@@ -9,130 +9,135 @@ class ProfileDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.topCenter,
-      child: Container(
-        height: 228,
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 8),
-            Container(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 48,
-                    height: 48,
-                    child: const CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                    ),
-                  ),
-                  const SizedBox(width: 12),
-                  GestureDetector(
-                    behavior: HitTestBehavior.opaque,
-                    onTap: () {
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute<QinAccountPage>(
-                            builder: (BuildContext context) {
-                          return QinAccountPage();
-                        }),
-                      );
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const <Widget>[
-                        Text(
-                          'しまぶー',
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF070417),
-                            decoration: TextDecoration.none,
-                          ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            child: Column(
+              children: <Widget>[
+                const SizedBox(height: 8),
+                Container(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 48,
+                        height: 48,
+                        child: const CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
                         ),
-                        Text(
-                          '@shimabuit',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Color(0xFFC2C6D2),
-                            decoration: TextDecoration.none,
-                          ),
+                      ),
+                      const SizedBox(width: 12),
+                      GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        onTap: () {
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute<QinAccountPage>(
+                                builder: (BuildContext context) {
+                              return QinAccountPage();
+                            }),
+                          );
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const <Widget>[
+                            Text(
+                              'しまぶー',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF070417),
+                                decoration: TextDecoration.none,
+                              ),
+                            ),
+                            Text(
+                              '@shimabuit',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Color(0xFFC2C6D2),
+                                decoration: TextDecoration.none,
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 0),
-            ),
-            DialogOption(
-              onTap: () {
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute<SettingPage>(
-                      builder: (BuildContext context) {
-                    return SettingPage();
-                  }),
-                );
-              },
-              icon: const Icon(Icons.settings_outlined),
-              text: const Text(
-                '設定',
-                style: TextStyle(
-                  color: Color(0xFF070417),
-                  fontSize: 14,
-                  decoration: TextDecoration.none,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 0),
                 ),
-              ),
-            ),
-            DialogOption(
-              onTap: () => print('help'),
-              icon: const Icon(Icons.help_outline_outlined),
-              text: const Text(
-                'ヘルプ',
-                style: TextStyle(
-                  color: Color(0xFF070417),
-                  fontSize: 14,
-                  decoration: TextDecoration.none,
-                ),
-              ),
-            ),
-            DialogOption(
-              onTap: () async => <void>{
-                Navigator.of(context).pop(),
-                await showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return CustomAlertDialog(
-                      title: 'ログアウト',
-                      subText: 'ログアウトしますか？',
-                      actionText: 'ログアウト',
-                      action: () => print('logout'),
+                DialogOption(
+                  onTap: () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute<SettingPage>(
+                          builder: (BuildContext context) {
+                        return SettingPage();
+                      }),
                     );
                   },
-                )
-              },
-              icon: const Icon(
-                Icons.logout,
-                color: Color(0xFFEF4444),
-              ),
-              text: const Text(
-                'ログアウト',
-                style: TextStyle(
-                  color: Color(0xFFEF4444),
-                  fontSize: 14,
-                  decoration: TextDecoration.none,
+                  icon: const Icon(Icons.settings_outlined),
+                  text: const Text(
+                    '設定',
+                    style: TextStyle(
+                      color: Color(0xFF070417),
+                      fontSize: 14,
+                      decoration: TextDecoration.none,
+                    ),
+                  ),
                 ),
-              ),
+                DialogOption(
+                  onTap: () => print('help'),
+                  icon: const Icon(Icons.help_outline_outlined),
+                  text: const Text(
+                    'ヘルプ',
+                    style: TextStyle(
+                      color: Color(0xFF070417),
+                      fontSize: 14,
+                      decoration: TextDecoration.none,
+                    ),
+                  ),
+                ),
+                DialogOption(
+                  onTap: () async => <void>{
+                    Navigator.of(context).pop(),
+                    await showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return CustomAlertDialog(
+                          title: 'ログアウト',
+                          subText: 'ログアウトしますか？',
+                          actionText: 'ログアウト',
+                          action: () => print('logout'),
+                        );
+                      },
+                    )
+                  },
+                  icon: const Icon(
+                    Icons.logout,
+                    color: Color(0xFFEF4444),
+                  ),
+                  text: const Text(
+                    'ログアウト',
+                    style: TextStyle(
+                      color: Color(0xFFEF4444),
+                      fontSize: 14,
+                      decoration: TextDecoration.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+              ],
             ),
-            const SizedBox(height: 8),
-          ],
-        ),
-        margin: const EdgeInsets.only(top: 100, left: 16, right: 16),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 24),
+            margin: const EdgeInsets.only(top: 100, left: 16, right: 16),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 24),
+          ),
+        ],
       ),
     );
   }
