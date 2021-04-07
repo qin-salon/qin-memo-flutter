e2188f91912c687b58a2b5a5303f02d192830e3a
abc123931 <abc123931@gmail.com>
2021-04-07 11:44:57 +0900

    feat: Update  profile form.

--- lib/qin_account_profile_page.dart
+++ lib/qin_account_profile_page.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:qin_memo/models/user_model.dart';
+import 'package:qin_memo/providers/user_provider.dart';
 
-class QinAccountProfilePage extends StatelessWidget {
+final StateProvider<String> nameStateProvider =
+    StateProvider<String>((ProviderReference ref) {
+  final User? user = ref.watch<User?>(userProvider);
+  if (user == null) {
+    return '';
+  }
+  return user.name;
+});
+
+final StateProvider<String> userNameStateProvider =
+    StateProvider<String>((ProviderReference ref) => '');
+
+class QinAccountProfilePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final StateController<String> nameState = useProvider(nameStateProvider);
+    final StateController<String> userNameState =
+        useProvider(userNameStateProvider);
+    final User? user = useProvider(userProvider);
+    final UserNotifier userNotifier = useProvider(userProvider.notifier);
+
+    final TextEditingController _nameController = useTextEditingController();
+    final TextEditingController _userNameController =
+        useTextEditingController();
+    _nameController.text = nameState.state;
+    _nameController.selection = TextSelection.fromPosition(
+      TextPosition(offset: _nameController.text.length),
+    );
+    _userNameController.text = userNameState.state;
+    _userNameController.selection = TextSelection.fromPosition(
+      TextPosition(offset: _userNameController.text.length),
+    );
+
     return Scaffold(
         appBar: AppBar(
           titleSpacing: 0,
@@ -39,8 +73,8 @@ class QinAccountProfilePage extends StatelessWidget {
                       Container(
                         width: 100,
                         height: 100,
-                        child: const Image(
-                          image: NetworkImage(
+                        child: Image(
+                          image: NetworkImage(user?.avatarUrl ??
                               'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
                         ),
                       ),
@@ -65,27 +99,36 @@ class QinAccountProfilePage extends StatelessWidget {
                   Container(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const <Widget>[
-                        Text(
+                      children: <Widget>[
+                        const Text(
                           '名前',
                           style: TextStyle(
                               fontSize: 14,
                               color: Color(0xFFC2C6D2),
                               fontWeight: FontWeight.bold),
                         ),
-                        SizedBox(height: 8),
+                        const SizedBox(height: 8),
                         TextField(
-                          decoration: InputDecoration(
-                              contentPadding: EdgeInsets.symmetric(
-                                  vertical: 13, horizontal: 16),
-                              border: OutlineInputBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(16)),
-                                  borderSide: BorderSide.none),
-                              hintText: '名前',
-                              hintStyle: TextStyle(color: Color(0xFFC2C6D2)),
-                              filled: true,
-                              fillColor: Color(0xFFF1F5F9)),
+                          controller: _nameController,
+                          onChanged: (String value) {
+                            nameState.state = value;
+                          },
+                          decoration: const InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 13,
+                              horizontal: 16,
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(16),
+                              ),
+                              borderSide: BorderSide.none,
+                            ),
+                            hintText: '名前',
+                            hintStyle: TextStyle(color: Color(0xFFC2C6D2)),
+                            filled: true,
+                            fillColor: Color(0xFFF1F5F9),
+                          ),
                         ),
                       ],
                     ),
@@ -94,30 +137,35 @@ class QinAccountProfilePage extends StatelessWidget {
                   Container(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const <Widget>[
-                        Text(
+                      children: <Widget>[
+                        const Text(
                           'ユーザー名',
                           style: TextStyle(
                               fontSize: 14,
                               color: Color(0xFFC2C6D2),
                               fontWeight: FontWeight.bold),
                         ),
-                        SizedBox(height: 8),
+                        const SizedBox(height: 8),
                         TextField(
-                          decoration: InputDecoration(
-                              contentPadding: EdgeInsets.symmetric(
-                                  vertical: 13, horizontal: 16),
-                              border: OutlineInputBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(16)),
-                                  borderSide: BorderSide.none),
-                              hintText: 'ユーザー名',
-                              hintStyle: TextStyle(color: Color(0xFFC2C6D2)),
-                              filled: true,
-                              fillColor: Color(0xFFF1F5F9)),
+                          controller: _userNameController,
+                          onChanged: (String value) {
+                            userNameState.state = value;
+                          },
+                          decoration: const InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 13, horizontal: 16),
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(16)),
+                                borderSide: BorderSide.none),
+                            hintText: 'ユーザー名',
+                            hintStyle: TextStyle(color: Color(0xFFC2C6D2)),
+                            filled: true,
+                            fillColor: Color(0xFFF1F5F9),
+                          ),
                         ),
-                        SizedBox(height: 8),
-                        Text(
+                        const SizedBox(height: 8),
+                        const Text(
                           'https://memo.qin.page/shimabu',
                           style: TextStyle(
                             fontSize: 14,
@@ -143,7 +191,14 @@ class QinAccountProfilePage extends StatelessWidget {
                         onPrimary: Colors.white,
                         shape: const StadiumBorder(),
                       ),
-                      onPressed: () {},
+                      onPressed: () async {
+                        try {
+                          await userNotifier.update(name: nameState.state);
+                          print('こうしんしました');
+                        } catch (e) {
+                          print(e);
+                        }
+                      },
                     ),
                   ),
                 ],
