c40b9b3820b8a1667264091e225406e2028259d8
abc123931 <abc123931@gmail.com>
2021-04-10 16:49:49 +0900

    feat: Update profile form.

--- lib/qin_account_profile_page.dart
+++ lib/qin_account_profile_page.dart
@@ -17,6 +17,8 @@ final StateProvider<String> userNameStateProvider =
     StateProvider<String>((ProviderReference ref) => '');
 
 class QinAccountProfilePage extends HookWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     final StateController<String> nameState = useProvider(nameStateProvider);
@@ -24,18 +26,6 @@ class QinAccountProfilePage extends HookWidget {
         useProvider(userNameStateProvider);
     final User? user = useProvider(userProvider);
     final UserNotifier userNotifier = useProvider(userProvider.notifier);
-
-    final TextEditingController _nameController = useTextEditingController();
-    final TextEditingController _userNameController =
-        useTextEditingController();
-    _nameController.text = nameState.state;
-    _nameController.selection = TextSelection.fromPosition(
-      TextPosition(offset: _nameController.text.length),
-    );
-    _userNameController.text = userNameState.state;
-    _userNameController.selection = TextSelection.fromPosition(
-      TextPosition(offset: _userNameController.text.length),
-    );
 
     return Scaffold(
         appBar: AppBar(
@@ -56,152 +46,170 @@ class QinAccountProfilePage extends HookWidget {
               const Text('プロフィール',
                   style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
               const SizedBox(height: 32),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  const Text(
-                    'アイコン',
-                    style: TextStyle(
-                        fontSize: 14,
-                        color: Color(0xFFC2C6D2),
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 8),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        width: 100,
-                        height: 100,
-                        child: Image(
-                          image: NetworkImage(user?.avatarUrl ??
-                              'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+              Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Text(
+                      'アイコン',
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Color(0xFFC2C6D2),
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 8),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          width: 100,
+                          height: 100,
+                          child: Image(
+                            image: NetworkImage(user?.avatarUrl ??
+                                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                          ),
                         ),
+                        const SizedBox(width: 24),
+                        ElevatedButton(
+                          child: const Text(
+                            '変更する',
+                            style: TextStyle(
+                                fontSize: 12, fontWeight: FontWeight.bold),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            primary: const Color(0xFFF1F5F9),
+                            onPrimary: Colors.black,
+                            shape: const StadiumBorder(),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 24),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          const Text(
+                            '名前',
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Color(0xFFC2C6D2),
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 8),
+                          TextFormField(
+                            initialValue: nameState.state,
+                            decoration: const InputDecoration(
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: 13,
+                                horizontal: 16,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(16),
+                                ),
+                                borderSide: BorderSide.none,
+                              ),
+                              hintText: '名前',
+                              hintStyle: TextStyle(color: Color(0xFFC2C6D2)),
+                              filled: true,
+                              fillColor: Color(0xFFF1F5F9),
+                            ),
+                            validator: (String? value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              return null;
+                            },
+                            onSaved: (String? value) {
+                              if (value != null) {
+                                nameState.state = value;
+                              }
+                            },
+                          ),
+                        ],
                       ),
-                      const SizedBox(width: 24),
-                      ElevatedButton(
+                    ),
+                    const SizedBox(height: 24),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          const Text(
+                            'ユーザー名',
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Color(0xFFC2C6D2),
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 8),
+                          TextField(
+                            onChanged: (String value) {
+                              userNameState.state = value;
+                            },
+                            decoration: const InputDecoration(
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 13, horizontal: 16),
+                              border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(16)),
+                                  borderSide: BorderSide.none),
+                              hintText: 'ユーザー名',
+                              hintStyle: TextStyle(color: Color(0xFFC2C6D2)),
+                              filled: true,
+                              fillColor: Color(0xFFF1F5F9),
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          const Text(
+                            'https://memo.qin.page/shimabu',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Color(0xFFC2C6D2),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 36),
+                    Container(
+                      width: double.infinity,
+                      child: ElevatedButton(
                         child: const Text(
-                          '変更する',
+                          '保存する',
                           style: TextStyle(
-                              fontSize: 12, fontWeight: FontWeight.bold),
+                              fontSize: 16, fontWeight: FontWeight.bold),
                         ),
                         style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(vertical: 18),
                           elevation: 0,
-                          primary: const Color(0xFFF1F5F9),
-                          onPrimary: Colors.black,
+                          primary: const Color(0xFF3B82F6),
+                          onPrimary: Colors.white,
                           shape: const StadiumBorder(),
                         ),
-                        onPressed: () {},
+                        onPressed: () async {
+                          try {
+                            final FormState? currentState =
+                                _formKey.currentState;
+                            if (currentState == null) {
+                              return;
+                            }
+                            if (currentState.validate()) {
+                              currentState.save();
+                              await userNotifier.update(name: nameState.state);
+                              print('こうしんしました');
+                            }
+                          } catch (e) {
+                            print(e);
+                          }
+                        },
                       ),
-                    ],
-                  ),
-                  const SizedBox(height: 24),
-                  Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        const Text(
-                          '名前',
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Color(0xFFC2C6D2),
-                              fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: 8),
-                        TextField(
-                          controller: _nameController,
-                          onChanged: (String value) {
-                            nameState.state = value;
-                          },
-                          decoration: const InputDecoration(
-                            contentPadding: EdgeInsets.symmetric(
-                              vertical: 13,
-                              horizontal: 16,
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(16),
-                              ),
-                              borderSide: BorderSide.none,
-                            ),
-                            hintText: '名前',
-                            hintStyle: TextStyle(color: Color(0xFFC2C6D2)),
-                            filled: true,
-                            fillColor: Color(0xFFF1F5F9),
-                          ),
-                        ),
-                      ],
                     ),
-                  ),
-                  const SizedBox(height: 24),
-                  Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        const Text(
-                          'ユーザー名',
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Color(0xFFC2C6D2),
-                              fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: 8),
-                        TextField(
-                          controller: _userNameController,
-                          onChanged: (String value) {
-                            userNameState.state = value;
-                          },
-                          decoration: const InputDecoration(
-                            contentPadding: EdgeInsets.symmetric(
-                                vertical: 13, horizontal: 16),
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(16)),
-                                borderSide: BorderSide.none),
-                            hintText: 'ユーザー名',
-                            hintStyle: TextStyle(color: Color(0xFFC2C6D2)),
-                            filled: true,
-                            fillColor: Color(0xFFF1F5F9),
-                          ),
-                        ),
-                        const SizedBox(height: 8),
-                        const Text(
-                          'https://memo.qin.page/shimabu',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Color(0xFFC2C6D2),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 36),
-                  Container(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      child: const Text(
-                        '保存する',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(vertical: 18),
-                        elevation: 0,
-                        primary: const Color(0xFF3B82F6),
-                        onPrimary: Colors.white,
-                        shape: const StadiumBorder(),
-                      ),
-                      onPressed: () async {
-                        try {
-                          await userNotifier.update(name: nameState.state);
-                          print('こうしんしました');
-                        } catch (e) {
-                          print(e);
-                        }
-                      },
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               )
             ],
           ),
