96aac417f7be8773415ed3fb3aa7b2b3d565a757
abc123931 <abc123931@gmail.com>
2021-04-14 10:48:02 +0900

    feat: Update bottom sheet by public.

--- lib/note/note_page_bottom_sheet.dart
+++ lib/note/note_page_bottom_sheet.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:qin_memo/dialog/custom_alert_dialog.dart';
+import 'package:qin_memo/models/note_model.dart';
 import 'package:qin_memo/providers/notes_provider.dart';
 
 class NotePageBottomSheet extends HookWidget {
@@ -12,6 +13,8 @@ class NotePageBottomSheet extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final NotesNotifier notifier = useProvider(notesProvider.notifier);
+    final Note? note = notifier.getNoteFromState(noteId);
+    final bool isPublic = note?.public != null && note?.public == true;
 
     return Container(
       child: Wrap(
@@ -73,16 +76,16 @@ class NotePageBottomSheet extends HookWidget {
                                           MainAxisAlignment.spaceBetween,
                                       crossAxisAlignment:
                                           CrossAxisAlignment.center,
-                                      children: const <Widget>[
+                                      children: <Widget>[
                                         Text(
-                                          '公開する',
-                                          style: TextStyle(
+                                          isPublic ? '非公開にする' : '公開する',
+                                          style: const TextStyle(
                                             fontSize: 14,
                                             fontWeight: FontWeight.bold,
                                             color: Color(0xFF3B82F6),
                                           ),
                                         ),
-                                        Icon(
+                                        const Icon(
                                           Icons.remove_red_eye_outlined,
                                           color: Color(0xFF3B82F6),
                                         ),
@@ -148,10 +151,12 @@ class NotePageBottomSheet extends HookWidget {
                         borderRadius: BorderRadius.circular(16),
                       ),
                     ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 16),
-                      child: Text('以下は公開後に操作ができます'),
-                    ),
+                    if (!isPublic)
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 16),
+                        child: Text('以下は公開後に操作ができます'),
+                      ),
+                    if (isPublic) const SizedBox(height: 16),
                     Container(
                       child: Column(
                         children: <Widget>[
@@ -164,18 +169,22 @@ class NotePageBottomSheet extends HookWidget {
                                         MainAxisAlignment.spaceBetween,
                                     crossAxisAlignment:
                                         CrossAxisAlignment.center,
-                                    children: const <Widget>[
+                                    children: <Widget>[
                                       Text(
                                         'Twitterでシェアする',
                                         style: TextStyle(
                                           fontSize: 14,
                                           fontWeight: FontWeight.bold,
-                                          color: Color(0xFFC2C6D2),
+                                          color: isPublic
+                                              ? Colors.black
+                                              : const Color(0xFFC2C6D2),
                                         ),
                                       ),
                                       Icon(
-                                        Icons.star_outline,
-                                        color: Color(0xFFC2C6D2),
+                                        Icons.star,
+                                        color: isPublic
+                                            ? const Color(0xFF1DA1F2)
+                                            : const Color(0xFFC2C6D2),
                                       ),
                                     ],
                                   ),
@@ -193,18 +202,22 @@ class NotePageBottomSheet extends HookWidget {
                                         MainAxisAlignment.spaceBetween,
                                     crossAxisAlignment:
                                         CrossAxisAlignment.center,
-                                    children: const <Widget>[
+                                    children: <Widget>[
                                       Text(
                                         '画像化してシェアする',
                                         style: TextStyle(
                                           fontSize: 14,
                                           fontWeight: FontWeight.bold,
-                                          color: Color(0xFFC2C6D2),
+                                          color: isPublic
+                                              ? Colors.black
+                                              : const Color(0xFFC2C6D2),
                                         ),
                                       ),
                                       Icon(
-                                        Icons.star_border_outlined,
-                                        color: Color(0xFFC2C6D2),
+                                        Icons.star,
+                                        color: isPublic
+                                            ? const Color(0xFF1DA1F2)
+                                            : const Color(0xFFC2C6D2),
                                       ),
                                     ],
                                   ),
@@ -226,18 +239,22 @@ class NotePageBottomSheet extends HookWidget {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const <Widget>[
+                        children: <Widget>[
                           Text(
                             'リンクをコピーする',
                             style: TextStyle(
                               fontSize: 14,
                               fontWeight: FontWeight.bold,
-                              color: Color(0xFFC2C6D2),
+                              color: isPublic
+                                  ? Colors.black
+                                  : const Color(0xFFC2C6D2),
                             ),
                           ),
                           Icon(
                             Icons.copy,
-                            color: Color(0xFFC2C6D2),
+                            color: isPublic
+                                ? Colors.black
+                                : const Color(0xFFC2C6D2),
                           ),
                         ],
                       ),
@@ -255,18 +272,22 @@ class NotePageBottomSheet extends HookWidget {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const <Widget>[
+                        children: <Widget>[
                           Text(
                             'その他のオプション',
                             style: TextStyle(
                               fontSize: 14,
                               fontWeight: FontWeight.bold,
-                              color: Color(0xFFC2C6D2),
+                              color: isPublic
+                                  ? Colors.black
+                                  : const Color(0xFFC2C6D2),
                             ),
                           ),
                           Icon(
                             Icons.more_horiz,
-                            color: Color(0xFFC2C6D2),
+                            color: isPublic
+                                ? Colors.black
+                                : const Color(0xFFC2C6D2),
                           ),
                         ],
                       ),
