6a2b56fcb01fb9fa4515350292724f771b1aee41
abc123931 <abc123931@gmail.com>
2021-04-26 11:44:31 +0900

    feat: Update excerpt.

--- lib/note/note_list.dart
+++ lib/note/note_list.dart
@@ -16,74 +16,104 @@ class NoteList extends StatelessWidget {
       },
       itemCount: notes.length,
       itemBuilder: (BuildContext context, int index) {
-        return GestureDetector(
-          onTap: () => <void>{
-            Navigator.of(context).push<NotePage>(
-                MaterialPageRoute<NotePage>(builder: (BuildContext context) {
-              return NotePage(note: notes[index]);
-            }))
-          },
-          child: Container(
-            child: Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    notes[index].excerpt ?? '',
-                    style: const TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  const Text('ほそく'),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        notes[index].updatedOn != null
-                            ? DateFormat('yyyy/MM/dd')
-                                .format(DateTime.parse(notes[index].updatedOn!))
-                            : '不明',
-                        style: const TextStyle(color: Color(0xFFC2C6D2)),
-                      ),
-                      if (notes[index].public)
-                        Container(
-                          child: const Text(
-                            '公開中',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 4, horizontal: 12),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(9999),
-                            color: const Color(0xFFFB923C),
-                          ),
-                        ),
-                    ],
-                  )
-                ],
-              ),
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  color: const Color(0xFFF1F5F9),
-                  boxShadow: const <BoxShadow>[
-                    BoxShadow(
-                        color: Color.fromRGBO(0, 0, 0, 0.12),
-                        offset: Offset(0, 1),
-                        blurRadius: 1)
-                  ]),
-            ),
-          ),
+        return NoteItem(
+          note: notes[index],
         );
       },
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({required this.note});
+
+  final Note note;
+
+  List<String> _createExcerpt(String text) {
+    final List<String> excerpts =
+        text.split('\n').where((String value) => value.isNotEmpty).toList();
+    if (excerpts.length <= 1) {
+      return <String>[excerpts[0], ''];
+    }
+    return excerpts;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> excerpts = _createExcerpt(note.excerpt ?? '');
+
+    return GestureDetector(
+      onTap: () => <void>{
+        Navigator.of(context).push<NotePage>(
+            MaterialPageRoute<NotePage>(builder: (BuildContext context) {
+          return NotePage(note: note);
+        }))
+      },
+      child: Container(
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                excerpts[0],
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                excerpts[1],
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    note.updatedOn != null
+                        ? DateFormat('yyyy/MM/dd')
+                            .format(DateTime.parse(note.updatedOn!))
+                        : '不明',
+                    style: const TextStyle(color: Color(0xFFC2C6D2)),
+                  ),
+                  if (note.public)
+                    Container(
+                      child: const Text(
+                        '公開中',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 4, horizontal: 12),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(9999),
+                        color: const Color(0xFFFB923C),
+                      ),
+                    ),
+                ],
+              )
+            ],
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: const Color(0xFFF1F5F9),
+              boxShadow: const <BoxShadow>[
+                BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.12),
+                    offset: Offset(0, 1),
+                    blurRadius: 1)
+              ]),
+        ),
+      ),
     );
   }
 }
