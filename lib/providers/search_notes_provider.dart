683682a77292b70cdfaba312d1fa9c6b62eddbb5
abc123931 <abc123931@gmail.com>
2021-05-02 23:12:35 +0900

    refactor: Refactor search notes provider.

--- lib/providers/search_notes_provider.dart
+++ lib/providers/search_notes_provider.dart
@@ -1,28 +1,25 @@
-import 'dart:convert';
-
+import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:http/http.dart' as http;
 import 'package:qin_memo/models/note_model.dart';
-import 'package:qin_memo/models/search_notes_response.dart';
 import 'package:qin_memo/providers/constants.dart';
 import 'package:qin_memo/providers/search_state_provider.dart';
 
-final AutoDisposeFutureProviderFamily<List<Note>, String> searchNotesProvider =
-    FutureProvider.autoDispose.family<List<Note>, String>(
+final searchNotesProvider = FutureProvider.autoDispose
+    .family<List<Note>, String>(
         (AutoDisposeProviderReference ref, String userId) async {
   final String search = ref.watch(searchStateProvider).state;
+
   if (search == '') {
     return <Note>[];
   }
 
-  final Uri uri =
-      Uri.parse('$API_ORIGIN/v1/users/$userId/notes/search?q=$search');
-  final http.Response response = await http.get(uri);
-  if (response.statusCode != 200) {
+  final Response<dynamic> response = await Dio()
+      .get<dynamic>('$API_ORIGIN/v1/users/$userId/notes/search?q=$search');
+
+  if (response.statusCode != 200 || response.data == null) {
     throw Exception('Failed to fetch search notes.');
   }
-  final Map<String, dynamic> decodedResponse = <String, dynamic>{
-    'notes': json.decode(response.body)
-  };
-  return SearchNotesResponse.fromJson(decodedResponse).notes;
+  final json = (response.data as List).cast<Map<String, dynamic>>();
+  final list = json.map((e) => Note.fromJson(e)).toList();
+  return list;
 });
